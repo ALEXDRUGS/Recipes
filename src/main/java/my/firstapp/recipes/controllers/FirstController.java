817e144ab8831c1fdfc3a993d168d816259c5970
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-12-15 18:25:51 +0300

    master

---
+++ src/main/java/my/firstapp/recipes/controllers/FirstController.java
@@ -0,0 +1,22 @@
+package my.firstapp.recipes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FirstController {
+    @GetMapping
+    public String helloWorld() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("path/to/info")
+    public String info() {
+        String studentsName = "Student's name: Alex";
+        String projectName = "Project name: Recipes";
+        String creationDate = "Date of creation: 15.12.2022";
+        String description = "This app works like a book of recipes";
+        return studentsName + " " + projectName + " " + creationDate + " " + description;
+    }
+}
